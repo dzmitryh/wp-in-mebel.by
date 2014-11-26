ae7e45398994cec652d91285f1609796ad764c97
Dzmitry Hubin <dzmitry_hubin@epam.com>
2014-11-26 23:38:43 +0300

    custom in-mebel.by plugin added

---
+++ wp-content/plugins/in-mebel-by-common/in-mebel-by-common.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ Plugin Name: in-mebel.by common things plugin
+ Description: This plugin handles some custom elements for in-mebel.by
+ Author: Dzmitry H
+ Version: 1.0
+ Requires at least: 3.8
+ License: GPL v3
+
+ Copyright (C) 2008-2014, Dzmitry H - dzmitry.hubin@gmail.com
+ */
+
+add_action( 'wp_head', 'handle_meta_tags');
+
+function handle_meta_tags() {
+	// yandex meta-tag..
+	echo "<meta name='yandex-verification' content='4347d186416b6498' />";
+}
+
+?>
